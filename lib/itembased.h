910f167b1eeb1ff87748ee2d5802e9d0c4c140e7
lcschv <lcs.chv@gmail.com>
2016-09-24 01:09:46 -0300

    ItemBased finished using weighted average concluded.

--- lib/itembased.h
+++ lib/itembased.h
@@ -2,16 +2,30 @@
 #define ITEMBASED_H
 #include "input.h"
 
+struct tuplaItemSimilarity{
+	int item;
+	float simi;
+};
+struct cmp {
+	bool operator()( const tuplaItemSimilarity & e1, const tuplaItemSimilarity & e2) {
+		return (e1.simi > e2.simi);
+	}
+};
+
+
 class ItemBasedRecommender{
 private:
 	LoadInput* loadinput;
 	vector<tuplaItemScore> ItemsLikedbyTargetUser;
 	vector<tuplaUserScore> targetvectorlist;
+	vector<tuplaItemSimilarity> KNN;
 public:
 	ItemBasedRecommender(LoadInput* loadinput);
 	float Similarity(int targetItem, tuplaItemScore item, const vector<tuplaUserScore> &targetvectorlist);
 	void Recommender();
-
+	void MyHeapPush(tuplaItemSimilarity truplas);
+	float WeightedAverage(int Targetuser);
+	float UserAverage(const vector<tuplaItemScore> &itemslist);
 };
 
 
--- src/itembased.cpp
+++ src/itembased.cpp
@@ -8,12 +8,15 @@
 ItemBasedRecommender::ItemBasedRecommender(LoadInput* loadinput){
     this->loadinput = loadinput;
     Recommender();
+    
 };
 
 
 
 void ItemBasedRecommender::Recommender(){
 	tupla nextTarget;
+	tuplaItemSimilarity tempTupla;
+	float r = 0;
 	// vector<int> ItemsLikedbyTargetUser;
 	// vector<int> UsersthatLikedTarget;
 	nextTarget = loadinput->getNextTarget();
@@ -25,12 +28,20 @@ void ItemBasedRecommender::Recommender(){
 		for (auto item:ItemsLikedbyTargetUser){
 			// cout << "Similaridade do target para o item "<<item<<endl;
 			float similaridade = Similarity(nextTarget.item , item, targetvectorlist);
+			tempTupla.item = item.item;
+			tempTupla.simi = similaridade;
+			MyHeapPush(tempTupla);
 			cout << "similaridades sao: "<< similaridade<<endl;
 		}
-		
+		if (!KNN.empty()){
+			r = WeightedAverage(nextTarget.user);
+		}else{
+			r = UserAverage(ItemsLikedbyTargetUser);
+		}
 		nextTarget = loadinput->getNextTarget();
 		ItemsLikedbyTargetUser.clear();
 		targetvectorlist.clear();
+		KNN.clear();
 	}
 };
 
@@ -38,6 +49,9 @@ void ItemBasedRecommender::Recommender(){
 float ItemBasedRecommender::Similarity(int targetItem, tuplaItemScore item, const vector<tuplaUserScore> &targetvectorlist){
 	vector<int> tempListofusers;
 	float denom_a, denom_b, dot;
+	denom_a = 0;
+	denom_b = 0;
+	dot = 0;
 	for (auto i:targetvectorlist){
 		denom_a += pow(i.rating,2);
 		if (loadinput->ItemsMap[item.item].find(i.user) != loadinput->ItemsMap[item.item].end()){
@@ -50,3 +64,41 @@ float ItemBasedRecommender::Similarity(int targetItem, tuplaItemScore item, cons
 	return dot / (sqrt(denom_a) * sqrt(denom_b));
 };
 
+
+
+void ItemBasedRecommender::MyHeapPush(tuplaItemSimilarity truplas) {
+	if (KNN.capacity() < 2 && truplas.simi != 0){
+		KNN.push_back(truplas);
+    	push_heap(KNN.begin(), KNN.end(), cmp());
+	} else if (truplas.simi != 0 && KNN.front().simi < truplas.simi){
+		KNN.erase(KNN.begin());
+		KNN.push_back(truplas);
+    	push_heap(KNN.begin(), KNN.end(), cmp());
+	} 
+};
+
+
+float ItemBasedRecommender::WeightedAverage(int Targetuser){
+	float predict, numerator ,denominator;
+	predict = 0;
+	numerator = 0;
+	denominator = 0;
+	for (auto it:KNN){
+		cout <<"item: "<< it.item <<"Simi: " << it.simi << "rating: "<< loadinput->ItemsMap[it.item][Targetuser]<<endl;
+		numerator += it.simi * loadinput->ItemsMap[it.item][Targetuser];
+		denominator += it.simi;
+	}
+	predict = numerator/denominator;
+	return predict;
+};
+
+float ItemBasedRecommender::UserAverage(const vector<tuplaItemScore> &itemslist){
+	float numerator, predict;
+	numerator = 0;
+	predict = 0;
+	for (auto it:itemslist){
+		numerator += it.rating;
+	}
+	predict = numerator / itemslist.size();
+	return predict;
+};
